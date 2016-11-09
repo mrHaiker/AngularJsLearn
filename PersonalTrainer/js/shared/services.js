36f16b0371b8ecdf23ab5da74e5a9410691c994a
Sergey <mr.haiker@mail.ru>
2016-11-09 23:55:48 +0200

    продолжение создания дериктив

--- PersonalTrainer/js/shared/services.js
+++ PersonalTrainer/js/shared/services.js
@@ -14,6 +14,7 @@ angular.module('app')
         var apiKey = null;
 
         this.configure = function (dbName) {
+            database = database;
             collectionsUrl = apiUrl + dbName + "/collections";
         }
 
@@ -25,7 +26,7 @@ angular.module('app')
             service.Exercises = $resource(collectionsUrl + "/exercises/:id", {}, { update: { method: 'PUT' } });
 
             service.getWorkouts = function () {
-                return $http.get(collectionsUrl + "/workouts", { params: {} })
+                return $http.get(collectionsUrl + "/workouts")
                         .then(function (response) {
                             return response.data.map(function (workout) {
                                 return new WorkoutPlan(workout);
@@ -34,7 +35,7 @@ angular.module('app')
             };
 
             service.getWorkout = function (name) {
-                return $q.all([service.Exercises.query().$promise, $http.get(collectionsUrl + "/workouts/" + name, { params: {} })])
+                return $q.all([service.Exercises.query().$promise, $http.get(collectionsUrl + "/workouts/" + name)])
                     .then(function (response) {
                         var allExercises = response[0];
                         var workout = new WorkoutPlan(response[1].data);
@@ -52,7 +53,7 @@ angular.module('app')
                         if (original) {
                             var workoutToSave = angular.copy(workout);
                             workoutToSave.exercises = workoutToSave.exercises.map(function (exercise) { return { name: exercise.details.name, duration: exercise.duration } });
-                            return $http.put(collectionsUrl + "/workouts/" + original.name, workoutToSave, { params: {} });
+                            return $http.put(collectionsUrl + "/workouts/" + original.name, workoutToSave);
                         }
                     })
                     .then(function (response) {
@@ -65,7 +66,7 @@ angular.module('app')
                     var workoutToSave = angular.copy(workout);
                     workoutToSave.exercises = workoutToSave.exercises.map(function (exercise) { return { name: exercise.details.name, duration: exercise.duration } });
                     workoutToSave._id = workoutToSave.name;
-                    return $http.post(collectionsUrl + "/workouts", workoutToSave, { params: {} })
+                    return $http.post(collectionsUrl + "/workouts", workoutToSave)
                                 .then(function (response) {
                                     return workout
                                 });
@@ -73,7 +74,7 @@ angular.module('app')
             }
 
             service.deleteWorkout = function (workoutName) {
-                return $http.delete(collectionsUrl + "/workouts/" + workoutName, { params: {} });
+                return $http.delete(collectionsUrl + "/workouts/" + workoutName);
             };
 
             return service;
@@ -85,20 +86,21 @@ angular.module('app')
         init();
     });
 
-angular.module('app').provider('ApiKeyAppenderInterceptor', function () {
-    var apiKey = null;
-    this.setApiKey = function (key) {
-        apiKey = key;
-    }
-    this.$get = ['$q', function ($q) {
-        return {
-            'request': function (config) {
-                if (apiKey && config && config.url.toLowerCase().indexOf("https://api.mongolab.com") >= 0) {
-                    config.params = config.params || {};
-                    config.params.apiKey = apiKey;
-                }
-                return config || $q.when(config);
-            }
+angular.module('app')
+    .provider('ApiKeyAppenderInterceptor', function () {
+        var apiKey = null;
+        this.setApiKey = function (key) {
+            apiKey = key;
         }
-    }];
-});
+        this.$get = ['$q', function ($q) {
+            return {
+                'request': function (config) {
+                    if (apiKey && config && config.url.toLowerCase().indexOf("https://api.mongolab.com") >= 0) {
+                        config.params = config.params || {};
+                        config.params.apiKey = apiKey;
+                    }
+                    return config || $q.when(config);
+                }
+            }
+        }];
+    });
